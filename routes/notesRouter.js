3073ffba47411ca48f41d96c08724d86f10bf47d
Will Berner <will.o.berner@gmail.com>
2021-08-17 23:00:35 -0400

    Notes can now be posted and retrieved consistently from the backend db.json file.

--- routes/notesRouter.js
+++ routes/notesRouter.js
@@ -1,14 +1,54 @@
 const express = require('express');
+const fs = require('fs');
+
+
 const tipsRouter = express.Router();
 
+const notes = require('../db/db.json');
+
 tipsRouter.get('/', (req, res) => {
-    console.log(`${req.method} request received for feedback`);
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(JSON.parse(data));
+        }
+    })
 
 });
 
 tipsRouter.post('/', (req, res) => {
-    console.log(req.body);
 
+    let currentNotes = null;
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        
+        if (err) {
+
+          console.error(err);
+
+        } else {
+            currentNotes = JSON.parse(data);
+
+            currentNotes.push({ "title": req.body.title, "text": req.body.text});
+
+            fs.writeFile("./db/db.json", JSON.stringify(currentNotes), (err) => {
+                
+                if (err) {
+
+                    console.log(err)
+                
+                } else {
+
+                    console.log("Note Added.");
+                    res.status(200).send(currentNotes);
+
+                }
+            });
+        }
+    });
 });
 
 module.exports = tipsRouter;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const notesRouter = require('./routes/notesRouter');
+const api = require('./routes/index.js');
 
 const app = express();
 
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/notes", notesRouter);
+app.use("/api", api);
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
